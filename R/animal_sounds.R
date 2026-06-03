063d58c981f89f002edf72ae001edea94a0a1305
Orville <odhombrebueno@nvsu.edu.ph>
2026-06-03 23:24:23 +0800

    session 2 done with 2nd "your turn"

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,16 @@
+#' Animal Sounds
+#'
+#' This is a function that gives back the animal and its
+#' sound [paste0()].
+#'
+#' @param animal What is the animal?
+#' @param sound What sound does the animal make?
+#'
+#' @returns A sentence that says "The animal goes sound!".
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "aw-aw")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
